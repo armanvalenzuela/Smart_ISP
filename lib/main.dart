4fa681bd0b3159e67ba51521c7806568ff9ddf90
Valenzuela, Arman Jo B. <arba.valenzuela.up@phinamed.com>
2025-09-12 16:38:27 +0800

    Removed Window Manager kasi nagstuck yong loading screen ng app

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:window_manager/window_manager.dart';
-
 import 'screens/widgets/splash_screen.dart';
 import 'features/auth/presentation/login_screen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await windowManager.ensureInitialized();
-
-//Adjust ur phone size here
-  WindowOptions windowOptions = const WindowOptions(
-    size: Size(390, 844), 
-    center: true,
-    backgroundColor: Colors.transparent,
-    titleBarStyle: TitleBarStyle.normal,
-  );
-
-  windowManager.waitUntilReadyToShow(windowOptions, () async {
-    await windowManager.show();
-    await windowManager.focus();
-
-    // Lock the window size
-    await windowManager.setResizable(false);
-    await windowManager.setMaximumSize(const Size(390, 844));
-    await windowManager.setMinimumSize(const Size(390, 844));
-  });
-
+void main(){
   runApp(const SmartISP());
 }
 
